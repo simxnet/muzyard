9ec76570b4e4cd1d5224194a4dd4d78cfb28bef3
simxnet <simxnet@envs.net>
2025-03-30 08:22:06 +0200

    feat: aye

---
+++ src/app/widget/[userId]/route.tsx
@@ -0,0 +1,142 @@
+import type { LanyardResponse } from "@/lib/types";
+import { ImageResponse } from "next/og";
+import type { NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+function songData(
+	start: number,
+	end: number,
+): { progress: string; duration: string; barWidth: number } {
+	if (start === 0 || end === 0)
+		return {
+			progress: "0:00",
+			duration: "0:00",
+			barWidth: 0,
+		};
+
+	const currentTime = Date.now();
+
+	const durationInMs = end - start;
+
+	function formatTime(ms: number): string {
+		const seconds = Math.floor(ms / 1000);
+		const minutes = Math.floor(seconds / 60);
+		const remainingSeconds = seconds % 60;
+		return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+	}
+
+	const progressInMs = currentTime - start;
+
+	const progressPercentage = Math.min((progressInMs / durationInMs) * 100, 100);
+
+	const barWidth = progressPercentage;
+
+	const formattedDuration = formatTime(durationInMs);
+	const formattedProgress = formatTime(progressInMs);
+
+	return {
+		progress: formattedProgress,
+		duration: formattedDuration,
+		barWidth: barWidth,
+	};
+}
+
+const themes = {
+	dark: {
+		bg: "#17171a",
+		fg: "#ffffff",
+		bar: "#27272a",
+		sub: "#b0b0b4",
+		accent: "#22e888",
+	},
+	light: {
+		bg: "#ffffff",
+		fg: "#191816",
+		bar: "#e2daf1",
+		sub: "#5b5a56",
+		accent: "#22e888",
+	},
+	catppuccin: {
+		bg: "#181926",
+		fg: "#cad3f5",
+		bar: "#24273a",
+		sub: "#b4baf3",
+		accent: "#7186fa",
+	},
+};
+
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: Promise<{ userId: string }> },
+) {
+	const searchParams = req.nextUrl.searchParams;
+	const theme = (searchParams.get("theme") as keyof typeof themes) ?? "dark";
+	const { userId } = await params;
+
+	const lanyardUser: LanyardResponse = await (
+		await fetch(`https://api.lanyard.rest/v1/users/${userId}`)
+	).json();
+
+	if (!lanyardUser.success)
+		return Response.json({ error: true, message: "invalid user" });
+
+	const spotify = lanyardUser.data.spotify;
+
+	const { duration, progress, barWidth } = songData(
+		spotify?.timestamps.start ?? 0,
+		spotify?.timestamps.end ?? 0,
+	);
+
+	const host = process.env.NEXT_PUBLIC_URL;
+
+	const currentTheme = themes[theme];
+
+	return new ImageResponse(
+		<div
+			style={{
+				backgroundImage: `url('${host}/bg.png')`,
+				backgroundColor: currentTheme.bg,
+				color: currentTheme.fg,
+			}}
+			tw="w-full h-full rounded-2xl p-12 flex flex-col bg-contain bg-no-repeat"
+		>
+			<div tw="flex flex-row items-center">
+				<img
+					alt="ok"
+					src={spotify?.album_art_url ?? `url('${host}/broken.png')`}
+					tw="w-44 h-44 rounded-2xl"
+				/>
+				<div tw="flex flex-col">
+					<h1 tw="ml-5 font-black leading-none">
+						{spotify?.song ?? "Not listening to anything"}
+					</h1>
+					<p tw="flex ml-5" style={{ color: currentTheme.sub }}>
+						By {spotify?.artist ?? "Nobody"}
+					</p>
+				</div>
+			</div>
+			<div tw="relative flex mt-10">
+				<div
+					tw="w-full h-3 rounded-md"
+					style={{ backgroundColor: currentTheme.bar }}
+				/>
+				<div
+					style={{
+						width: `${barWidth}%`,
+						backgroundColor: currentTheme.accent,
+					}}
+					tw="h-3 rounded-md absolute"
+				/>
+			</div>
+			<div tw="flex justify-between">
+				<p>{progress}</p>
+				<p>{duration}</p>
+			</div>
+		</div>,
+		{
+			width: 900,
+			height: 350,
+		},
+	);
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,22 @@
+export interface LanyardResponse {
+	data: Data;
+	success: boolean;
+}
+
+export interface Data {
+	spotify?: Spotify;
+}
+
+export interface Spotify {
+	timestamps: Timestamps;
+	album: string;
+	album_art_url: string;
+	artist: string;
+	song: string;
+	track_id: string;
+}
+
+export interface Timestamps {
+	start: number;
+	end: number;
+}
